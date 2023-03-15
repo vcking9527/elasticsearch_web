993c48996ab7188feecf020cc425682e385614b5
VCKing9527 <ccyen420@gmail.com.tw>
2023-03-16 01:21:25 +0800

    first init

---
+++ WebElasticSearch/Controllers/AllSearchController.cs
@@ -0,0 +1,177 @@
+﻿
+using System.Linq;
+using System.Web.Mvc;
+using WebElasticSearch.Connector;
+using WebElasticSearch.Models;
+
+namespace WebElasticSearch.Controllers
+{
+    public class AllSearchController : Controller
+    {
+        private readonly ConnectionToEs _connectionToEs;
+        public AllSearchController()
+        {
+            _connectionToEs = new ConnectionToEs();
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View("Search");
+        }
+
+
+        public JsonResult DataSearch(string jobtitle, string nationalIDNumber)
+        {
+
+            if (!string.IsNullOrEmpty(jobtitle) && !string.IsNullOrEmpty(nationalIDNumber))
+            {
+                var responsedata = _connectionToEs.EsClient().Search<Humanresources>(s => s
+                                 .Index("humanresources")
+                                 .Type("doc")
+                                 .Size(50)
+                                 .Query(q => q
+                                     .Match(m => m
+                                         .Field(f => f.jobtitle)
+                                         .Query(jobtitle)
+                                     )
+                                     && q
+                                     .Match(m => m
+                                         .Field(f => f.nationalidnumber)
+                                         .Query(nationalIDNumber)
+                                 ))
+                             );
+
+                var datasend = (from hits in responsedata.Hits
+                                select hits.Source).ToList();
+
+                return Json(new { datasend, responsedata.Took }, behavior: JsonRequestBehavior.AllowGet);
+
+            }
+            else if (!string.IsNullOrEmpty(jobtitle))
+            {
+                var responsedata = _connectionToEs.EsClient().Search<Humanresources>(s => s
+                                .Index("humanresources")
+                                .Type("doc")
+                                .Size(50)
+                                .Query(q => q
+                                    .Match(m => m
+                                        .Field(f => f.jobtitle)
+                                        .Query(jobtitle)
+                                    )));
+
+                var datasend = (from hits in responsedata.Hits
+                                select hits.Source).ToList();
+
+                return Json(new { datasend, responsedata.Took }, behavior: JsonRequestBehavior.AllowGet);
+            }
+            else if (!string.IsNullOrEmpty(nationalIDNumber))
+            {
+                var responsedata = _connectionToEs.EsClient().Search<Humanresources>(s => s
+                                .Index("humanresources")
+                                .Type("doc")
+                                .Size(50)
+                                .Query(q => q
+                                    .Match(m => m
+                                        .Field(f => f.nationalidnumber)
+                                        .Query(nationalIDNumber)
+                                    )));
+                var datasend = (from hits in responsedata.Hits
+                                select hits.Source).ToList();
+
+                return Json(new { datasend, responsedata.Took }, behavior: JsonRequestBehavior.AllowGet);
+            }
+            return Json(data: null, behavior: JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        #region Single Search
+        //public JsonResult DataSearch(string jobtitle, string nationalIDNumber)
+        //{
+        //    var responsedata = _connectionToEs.EsClient().Search<Humanresources>(s => s
+        //                            .Index("humanresources")
+        //                            .Type("doc")
+        //                            .Size(50)
+        //                            .Query(q => q
+        //                                .Match(m => m
+        //                                    .Field(f => f.jobtitle)
+        //                                    .Query(jobtitle)
+        //                                )
+        //                            )
+        //                        );
+
+        //    var datasend = (from hits in responsedata.Hits
+        //                    select hits.Source).ToList();
+
+        //    return Json(new { datasend, responsedata.Took }, behavior: JsonRequestBehavior.AllowGet);
+        //} 
+        #endregion
+
+        #region Conditional Search
+        //public JsonResult DataSearch(string jobtitle, string nationalIDNumber)
+        //{
+
+        //    if (!string.IsNullOrEmpty(jobtitle) && !string.IsNullOrEmpty(nationalIDNumber))
+        //    {
+        //        var responsedata = _connectionToEs.EsClient().Search<Humanresources>(s => s
+        //                         .Index("humanresources")
+        //                         .Type("doc")
+        //                         .Size(50)
+        //                         .Query(q => q
+        //                             .Match(m => m
+        //                                 .Field(f => f.jobtitle)
+        //                                 .Query(jobtitle)
+        //                             )
+        //                             && q
+        //                             .Match(m => m
+        //                                 .Field(f => f.nationalidnumber)
+        //                                 .Query(nationalIDNumber)
+        //                         ))
+        //                     );
+
+        //        var datasend = (from hits in responsedata.Hits
+        //                        select hits.Source).ToList();
+
+        //        return Json(new { datasend, responsedata.Took }, behavior: JsonRequestBehavior.AllowGet);
+
+        //    }
+        //    else if (!string.IsNullOrEmpty(jobtitle))
+        //    {
+        //        var responsedata = _connectionToEs.EsClient().Search<Humanresources>(s => s
+        //                        .Index("humanresources")
+        //                        .Type("doc")
+        //                        .Size(50)
+        //                        .Query(q => q
+        //                            .Match(m => m
+        //                                .Field(f => f.jobtitle)
+        //                                .Query(jobtitle)
+        //                            )));
+
+        //        var datasend = (from hits in responsedata.Hits
+        //                        select hits.Source).ToList();
+
+        //        return Json(new { datasend, responsedata.Took }, behavior: JsonRequestBehavior.AllowGet);
+        //    }
+        //    else if (!string.IsNullOrEmpty(nationalIDNumber))
+        //    {
+        //        var responsedata = _connectionToEs.EsClient().Search<Humanresources>(s => s
+        //                        .Index("humanresources")
+        //                        .Type("doc")
+        //                        .Size(50)
+        //                        .Query(q => q
+        //                            .Match(m => m
+        //                                .Field(f => f.nationalidnumber)
+        //                                .Query(nationalIDNumber)
+        //                            )));
+        //        var datasend = (from hits in responsedata.Hits
+        //                        select hits.Source).ToList();
+
+        //        return Json(new { datasend, responsedata.Took }, behavior: JsonRequestBehavior.AllowGet);
+        //    }
+        //    return Json(data: null, behavior: JsonRequestBehavior.AllowGet);
+
+        //} 
+        #endregion
+    }
+}
---
+++ WebElasticSearch/Models/Humanresources.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebElasticSearch.Models
+{
+    public class Humanresources
+    {
+        public string nationalidnumber { get; set; }
+        public string LoginID { get; set; }
+        public string OrganizationNode { get; set; }
+        public int? OrganizationLevel { get; set; }
+        public string jobtitle { get; set; }
+        public string BirthDate { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Gender { get; set; }
+        public DateTime? HireDate { get; set; }
+        public bool? SalariedFlag { get; set; }
+        public int? VacationHours { get; set; }
+        public int? SickLeaveHours { get; set; }
+        public bool? CurrentFlag { get; set; }
+        public string rowguid { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
